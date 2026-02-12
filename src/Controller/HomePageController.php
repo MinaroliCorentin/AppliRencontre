4079bfebc50a7907a5d9dde51edd9c73f9fb6e87
MinaroliCorentin <minaroli.corenin@gmail.com>
2026-02-12 09:52:09 +0100

    Push dossier Images + debut homePage

---
+++ src/Controller/HomePageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomePageController extends AbstractController
+{
+    //#[Route('/home/page', name: 'app_home_page')]
+    #[Route('/test-visuel', name: 'app_test_visuel')]
+    public function index(): Response
+    {
+        return $this->render('home_page/index.html.twig', [
+            'controller_name' => 'HomePageController',
+        ]);
+    }
+}
